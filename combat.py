8580abe78a44197f300064b9cce1b4cba2e39a79
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2024-10-07 01:48:31 +0000

    [feat] add combat module

---
+++ combat.py
@@ -0,0 +1,77 @@
+import time
+
+import character
+
+
+def player_combat(player: character.Player, enemy: character.Enemy) -> bool | int:
+    print("\n")
+    time.sleep(0.5)
+    crit = 1  #if there is no crit does not change
+
+    if player.gears["weapon"].crit():
+        crit = 2  # double the damage when it crits
+
+    damage = (player.gears['weapon'].attack + self.attack -
+              enemy.defense) * crit
+
+    if damage <= 0:
+        damage = 1
+
+    enemy.health -= damage
+
+    print(f"You dealt {damage} damage to the {enemy.name}.")
+
+    print(f"{enemy.name} current health:{enemy.health}")
+
+    if enemy.health <= 0:
+        enemy.health = 0
+        print(f"{enemy} fainted.")
+        if isinstance(enemy, Boss):
+            return -888
+        return True
+    else:
+        return False
+
+def enemy_combat(enemy: character.Enemy, player: character.Player) -> bool | int:
+    print("\n")
+    time.sleep(0.5)
+    damage = (enemy.attack - player.defense)  #enemy doesn't crit
+    
+    if damage < 0:
+        damage = 1
+    
+    player.health -= damage  #lose health
+    
+    print(f"You received {damage} damage from the {enemy.name}."
+          )  #print damage to player
+    
+    print(f"{player.name} current health:{player.health}")  #print hp left
+    
+    if player.health <= 0:
+        player.health = 0
+        print("You fainted. Skill Issue.")
+        return -1
+    else:
+        return False
+
+def boss_combat(boss: character.Boss, player: character.Player) -> bool | int:
+    print("\n")
+    time.sleep(0.5)
+    damage = (boss.attack - player.defense)  #enemy doesn't crit
+
+    if damage < 0:
+        damage = 1
+
+    player.health -= damage  #lose health
+
+    print(f"You received {damage} damage from the {boss.name}."
+          )  #print damage to player
+
+    print(f"{player.name} current health:{player.health}")  #print hp left
+
+    if player.health <= 0:
+        player.health = 0
+        print("You fainted. Skill Issue.")
+        return -666
+    else:
+        return False
